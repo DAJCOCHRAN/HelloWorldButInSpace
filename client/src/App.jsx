bc06404acfead1807e3e3f29f5f294d89a703bdd
ben-w-martin <ben.w.martin17@gmail.com>
2024-05-17 23:20:38 -0400

    transfering device

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from "./Home";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <>
       <Router>
+        <NavBar></NavBar>
         <Routes>
           <Route path="/" element={<Home />}></Route>
         </Routes>
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
-  return <>nav</>;
+  return (
+    <>
+      <header className="header">
+        <nav className="nav">
+          <ul className="nav-links">
+            <li className="nav__link bg-danger">
+              <Link>Home</Link>
+            </li>
+            <li className="nav__link">
+              <Link>Menu</Link>
+            </li>
+            <li className="nav__link">
+              <Link>About</Link>
+            </li>
+            <li className="nav__link">
+              <Link>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export { NavBar };
