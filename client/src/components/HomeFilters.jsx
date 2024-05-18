f66c780d005259ee58f56650580923c357d1b1d5
ben-w-martin <ben.w.martin17@gmail.com>
2024-05-18 15:23:11 -0400

    add: home page with card components

--- client/src/components/HomeFilters.jsx
+++ client/src/components/HomeFilters.jsx
@@ -1,7 +1,9 @@
 function HomeFilters() {
   return (
     <div className="home-grid-space home-filters">
-      <div className="home-filters__title">FILTERS</div>
+      <div className="home-filters__title">
+        <h2 className="heading-secondary">Choose what you see:</h2>
+      </div>
       <div className="home-filters__body">Checkboxes, inputs</div>
     </div>
   );
--- client/src/components/HomeView.jsx
+++ client/src/components/HomeView.jsx
@@ -1,10 +1,27 @@
-function HomeView() {
+import * as PropTypes from "prop-types";
+import { Card } from "./Card";
+Card;
+
+function HomeView(props) {
   return (
     <div className="home-grid-space home-view">
-      <div className="home-view__title">TITLE GOES HERE</div>
-      <div className="home-view__body">Api response cards</div>
+      <div className="home-view__title">
+        <h2 className="heading-secondary">
+          Happening in Space near:
+          {props.location
+            ? `${props.location.city} ${props.location.state}`
+            : " loading..."}
+        </h2>
+      </div>
+      <div className="home-view__body">
+        <Card></Card>
+      </div>
     </div>
   );
 }
+
+HomeView.propTypes = {
+  location: PropTypes.object,
+};
 
 export { HomeView };
